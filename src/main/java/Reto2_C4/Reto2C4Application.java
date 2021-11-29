8a891594dab828a3458cb3cfa140e3c1db584364
Gabriel Roa <gabrielfelipert@gmail.com>
2021-11-29 12:38:49 -0500

    Second commit

--- src/main/java/Reto2_C4/Reto2C4Application.java
+++ src/main/java/Reto2_C4/Reto2C4Application.java
@@ -1,13 +1,25 @@
 package Reto2_C4;
 
+import Reto2_C4.Interfaces.UserInterface;
+import Reto2_C4.Interfaces.CleaningProductsInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Reto2C4Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Reto2C4Application.class, args);
+public class Reto2C4Application implements CommandLineRunner {
+        @Autowired
+        private CleaningProductsInterface cleaningProductsInterface;
+        @Autowired
+        private UserInterface userInterface;
+            public static void main(String[] args) {
+                    SpringApplication.run(Reto2C4Application.class, args);
 	}
+        @Override
+        public void run (String... args) throws Exception{
+            cleaningProductsInterface.deleteAll();
+            userInterface.deleteAll();
+        }
 
 }
